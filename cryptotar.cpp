8a8ef2902d9338c839d858072b8f66ce6bf52078
LuKijaAHAH <ivan6711071@gmail.com>
2023-12-05 15:02:51 +0300

    del main.cpp

--- cryptotar.cpp
+++ cryptotar.cpp
@@ -2,18 +2,23 @@
 #include <getopt.h>
 #include <vector>
 
+#include "cryptotarlib.hpp"
+
 int  main(int argc, char *argv[]){
 
-	bool has_c, has_f, has_o = false;
-	std::vector <char*> files = {};
-	char* output_file_name = NULL;
+	std::cout << "popa" << std::endl;
+	bool has_h, has_u, has_c, has_f, has_o = false;
+	std::vector <std::string> paths;
+	char* output_file_name; char*  unpack_ctar;
 
 	int r, option_index = 0;
 
 
-	const char *short_options = "cf:o:";
+	const char *short_options = "hucf:o:";
 
 	const struct option long_options[] = {
+		{"help", no_argument, NULL, 'h'},
+		{"unpack", required_argument, NULL, 'u'},
 		{"compress", no_argument, NULL, 'c'},
 		{"files", required_argument, NULL, 'f'},
 		{"output", required_argument, NULL, 'o'},
@@ -24,17 +29,28 @@ int  main(int argc, char *argv[]){
 		
 		switch(r) {
 
+			case 'h':
+				has_h = true;
+				std::cout << "Help: -u (--unpack)\n-c (--compress) \n-f (--files) \n-o (--output)" << std::endl;
+				break;
+
+			case 'u':
+				has_u = true;
+				if (optarg != NULL){
+					unpack_ctar = argv[optind-1];
+				}
+				break;
+
+
 			case 'c':
 				has_c = true;
-				std::cout << "File be compress\n" << std::endl;
 				break;
 
 			case 'f':
 				has_f = true;
 				if (optarg != NULL){
-					for (int i = optind - 1; i < argc && *argv[i] != '-'; i++) files.push_back(argv[i]);
+					for (int i = optind - 1; i < argc && *argv[i] != '-'; i++) paths.push_back(argv[i]);
 				}
-
 				break;
 
 			case 'o':
@@ -42,18 +58,31 @@ int  main(int argc, char *argv[]){
 				if (optarg != NULL){
 					output_file_name = argv[optind-1];	
 				}
+				break;
 	 	}
 	}
 
 	//errors
-	if (has_c == false) { std::cout << "Not use flag -c (--copmress)" << std::endl; return -1;}
-	else if (has_f == false) {std::cout << "Not use flag -f (--files)" << std::endl; return -1;}
-	else if (has_o == false) {std::cout << "Not use flag -o (--output)" << std::endl; return -1;}
+	
+	if (has_h == true) {has_c == false; has_u == false; has_f == false; has_o == false;}
+	
+	if (has_u == true || (has_c == true && has_f == true && has_o)) {std::cout << "You cannot use -u (--unpack) with other flags" << std::endl; return -1;}
+	
+	if (has_h == false || has_u == false){
+		if (has_c == false) { std::cout << "Not use flag -c (--copmress)" << std::endl; return -1;}
+		else if (has_f == false) {std::cout << "Not use flag -f (--files)" << std::endl; return -1;}
+		else if (has_o == false) {std::cout << "Not use flag -o (--output)" << std::endl; return -1;}
+	}
 
-	//output
-	for ( int i = 0; i < files.size(); i++) std::cout << files[i] << std::endl;
-	std::cout << "\n" << output_file_name << std::endl;
+	//ctar_creating_with_vector
+	
+	if ( has_c = true ) {cryptotar cr(output_file_name, paths); cr.closeTar();}
+	
 
+	//unpack
+	
+	if ( has_u = true ) {cryptotar tarEx(unpack_ctar, ".");}
+	
 
 	return 0;
 }
